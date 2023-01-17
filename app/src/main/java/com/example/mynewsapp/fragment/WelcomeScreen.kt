27ea0b9ec814aa6f7dd03c6ff67aeb171f35f312
selamikadioglu <selami-kadioglu@outlook.com>
2023-01-17 16:25:09 +0300

    splash screen added

---
+++ app/src/main/java/com/example/mynewsapp/fragment/WelcomeScreen.kt
@@ -0,0 +1,17 @@
+package com.example.mynewsapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mynewsapp.R
+
+class WelcomeScreen : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_welcome_screen, container, false)
+    }
+}
